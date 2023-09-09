c1e55a72c12538544e7106687ea261feb989bf1e
Anoop Singh <48690990+anoop04singh@users.noreply.github.com>
2023-09-09 22:23:16 +0530

    Create Problem34.py

---
+++ Problem34.py
@@ -0,0 +1,25 @@
+#Problem Statement
+#Find the sum of all numbers below N which divide the sum of the factorial of their digits.
+
+#SOLUTION
+N = int(input()) #takesInput
+def factorial(n): #defined a function to find factorial
+    fact = 1
+    for i in range(1,n+1):
+        fact = fact*i
+    return fact
+
+def splitI(s): #defined a function to break a number into indiviual digits. For eg: 14 to [1,4]
+    a =[]
+    for i in str(s):
+        a.append(int(i))
+    return a
+
+out = []
+for i in range(11,N): #loop to find the requied numbers and sum them and check the if statement and returns the output
+    a = sum([factorial(x) for x in splitI(i)])
+    if a%i == 0:
+        out.append(i)
+    else:
+        pass
+print(sum(out))
